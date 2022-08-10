08d341ac140ce44d30849bc842521919c18152f8
Aliaksei Hrynko <aliaksei.hrynko@gmail.com>
2022-08-10 21:39:30 +0300

    Add i18n

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'i18n/locales',
+      enableLegacy: false,
+      runtimeOnly: false,
+      compositionOnly: false,
+      fullInstall: true,
+    },
+  },
 })
