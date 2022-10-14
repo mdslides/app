0e10d443575adc3ab97b847e03582117666e0ad3
Aliaksei Hrynko <aliaksei.hrynko@gmail.com>
2022-10-14 22:52:14 +0200

    Update logo

--- src/components/AppLogo.vue
+++ src/components/AppLogo.vue
@@ -1,36 +1,41 @@
 <template>
   <div class="app-logo">
     <div class="app-logo__icon">
-      <span>md</span>
+      <span>Md</span>
     </div>
 
-    <div class="app-logo__text">slides</div>
+    <div class="app-logo__text">
+      <span>Slides</span>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .app-logo {
   display: flex;
+  font-size: 1rem;
+  font-weight: 700;
   user-select: none;
 
   &__icon {
     display: flex;
     justify-content: flex-end;
     align-items: flex-end;
-    padding: 0.5rem;
+    padding: 0.5rem 0.3rem;
     width: 4rem;
     height: 3rem;
     background-color: var(--color-text);
-    font-size: 1rem;
-    font-weight: 800;
     color: var(--color-background);
   }
 
   &__text {
-    margin-left: 0.5rem;
-    line-height: 3rem;
-    font-size: 2rem;
-    font-weight: 500;
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-end;
+    padding: 0.5rem 0.2rem;
+    width: 4rem;
+    height: 3rem;
+    letter-spacing: -0.025em;
   }
 }
 </style>
