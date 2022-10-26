b85ac3ba91c710f389c5db9af7da6a7c4b721e86
Aliaksei Hrynko <aliaksei.hrynko@gmail.com>
2022-10-26 08:54:30 +0200

    Set env specific public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
+  publicPath: process.env.PUBLIC_PATH,
   transpileDependencies: true,
   pluginOptions: {
     i18n: {
